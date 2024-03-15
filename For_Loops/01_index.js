a28f254a1730d1ed61a0056e0b277b55a73afa82
Shubham Londhe <shubhamlondhe63@gmail.com>
2024-03-15 17:15:10 +0530

    object iteration

--- For_Loops/01_index.js
+++ For_Loops/01_index.js
@@ -1,34 +1,40 @@
 // for loop
 
-
-const myArray = ['shubham', 'hello', 'world']
+const myArray = ["shubham", "hello", "world"];
 for (let i = 0; i < myArray.length; i++) {
   const element = myArray[i];
-  console.log(element);
+//   console.log(element);
 }
 
-
-
-for (let i = 1; i< 10 ; i++){
-    console.log("Outer loop i value is :",  i);
-    for (let j = 1; j <=10; j++) {
-           console.log(i*j);
-    }   
+for (let i = 1; i < 10; i++) {
+//   console.log("Outer loop i value is :", i);
+  for (let j = 1; j <= 10; j++) {
+    // console.log(i * j);
+  }
 }
 
-// with object we cannot iterate througth the 
+// with object we cannot iterate througth the
+
 const myObject = {
-    firstName : "Shubham",
-    LastName : "Londhe",
-    age : 25
-}
+  firstName: "Shubham",
+  LastName: "Londhe",
+  age: 25,
+};
 
-const keys = Object.keys(myObject);
+const keys = Object.keys(myObject);  // this will return an array of keys 
 console.log("keys are : ", keys);
 
-for (let i = 0; i < keys.length; i++) {
-    const key = keys[i];
-    console.log(key + ": " + myObject[key]);
+for (let i = 0; i < keys.length; i++) {             // we are iterating to array here
+  const key = keys[i];
+//   console.log(key + ": " + myObject[key]);
 }
+
+
+// lets understand For Of loop 
+
+// for (const key of myObject) {
+//   console.log( key );         // for of loop does not work wih objects 
+// }
+
 
 
